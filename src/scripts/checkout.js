50dda525c749571188a29f06dcf885b4929f8602
Alias M6 <aliasmontoya719@gmail.com>
2016-07-27 18:02:14 -0600

    Update the Check Out page * Create states.js to populate states into  the drop down menu on doc ready * Add all required inputs * Set up event listeners to validate input fields for first and last name

--- src/scripts/checkout.js
+++ src/scripts/checkout.js
@@ -1,3 +1,36 @@
 $(document).on('ready', function() {
   console.log('GO TEAM - Checkout');
+  // $('.firstName').on('blur keydown', function() {
+  //   if ($('.firstName').val().length < 3 || $('.firstName').val() === "") {
+  //   $('.firstNameShipping').addClass('has-error');
+  //   $('.firstNameShipping').removeClass('has-success');
+  //   } else {
+  //   $('.firstNameShipping').addClass('has-success');
+  //   $('.firstNameShipping').removeClass('has-error');
+  //   }
+  // });
+  nameValidate('.firstName','.firstNameShipping');
+  nameValidate('.lastName','.lastNameShipping');
+  // $('.lastName').on('blur keydown', function(){
+  //   if ($('.lastName').val().length < 3 || $('.lastName').val() === "") {
+  //       $('.lastNameShipping').addClass('has-error');
+  //       $('.lastNameShipping').removeClass('has-success');
+  //   } else {
+  //       $('.lastNameShipping').addClass('has-success');
+  //       $('.lastNameShipping').removeClass('has-error');
+  //   }
+  // })
+
 });
+
+function nameValidate(input, labelClass){ $(input).on('blur keydown', function()
+    {
+    if ($(input).val().length < 3 || $(input).val() === "") {
+    $(labelClass).addClass('has-error');
+    $(labelClass).removeClass('has-success');
+    } else {
+    $(labelClass).addClass('has-success');
+    $(labelClass).removeClass('has-error');
+    }
+  });
+}

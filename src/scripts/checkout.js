bfa288364395c4f87efaeabd9a33cf4313d40791
Alias M6 <aliasmontoya719@gmail.com>
2016-07-29 09:02:00 -0600

    Create receipt and finish credit card validations

--- src/scripts/checkout.js
+++ src/scripts/checkout.js
@@ -1,24 +1,24 @@
 $(document).on('ready', function() {
   console.log('GO TEAM - Checkout');
-  // validate first name, last name, and address line 1 of shipping
-  textValidate('.firstNameS','.firstNameShipping');
-  textValidate('.lastNameS','.lastNameShipping');
-  textValidate('.addressS','.addressShipping');
-  textValidate('.cityS', '.cityShipping');
-  // validate state drop down of shipping
-  stateValidate('.dropDownS', '.stateShipping');
-  // validate zip of shipping
-  zipValidate('.zipS','.zipShipping');
-  // validate first name, last name, and address line 1 of billing
-  textValidate('.firstNameB','.firstNameBilling');
-  textValidate('.lastNameB','.lastNameBilling');
-  textValidate('.addressB','.addressBilling');
-  textValidate('.cityB', '.cityBilling');
-  // validate state drop down of billing
-  stateValidate('.dropDownB', '.stateBilling')
-  zipValidate('.zipB', '.zipBilling')
-});
+  nameCityAddressValidation();
+  stateValidations();
 
+  // stateValidate('.dropDownS', '.stateShipping');
+
+  zipValidate('.zipS','.zipShipping');
+
+  // stateValidate('.dropDownB', '.stateBilling')
+  // validate zip for Billing
+  zipValidate('.zipB', '.zipBilling')
+  // card validators
+  cardValidate('.cardNum', '.cardNumber');
+  CVCvalidate('.CVCnum','.CVCNumber');
+  expValidate('.expiration','.expirationDate');
+  //copy shipping info into billing
+  copyShipping();
+
+
+});
 
 
 
@@ -30,7 +30,7 @@ HELPER FUNCTIONS
 
 function textValidate(input, divClass) {
   $(input).on('blur keydown', function() {
-    if ($(input).val().length < 2) {
+    if ($(this).val().length < 2) {
       $(divClass).addClass('has-error');
       $(divClass).removeClass('has-success');
     } else {
@@ -55,10 +55,7 @@ function stateValidate(input, divClass) {
 }
 
 function zipValidate(input, divClass) {
-
-
   $(input).on('blur keydown', function() {
-
     if ($(this).val().length < 4 || $(this).val().length > 9) {
       $(divClass).addClass('has-error');
       $(divClass).removeClass('has-success');
@@ -70,3 +67,75 @@ function zipValidate(input, divClass) {
     }
   });
 }
+
+function cardValidate(input, divClass) {
+  $(input).on('blur keydown', function() {
+    if (Stripe.card.validateCardNumber($(input).val()) === true) {
+      $(divClass).addClass('has-success');
+      $(divClass).removeClass('has-error');
+    }
+    else {
+      $(divClass).addClass('has-error');
+      $(divClass).removeClass('has-success');
+    }
+  });
+}
+
+function CVCvalidate(input, divClass) {
+  $(input).on('blur input', function () {
+    if (Stripe.card.validateCVC($(input).val()) === true) {
+      $(divClass).addClass('has-success');
+      $(divClass).removeClass('has-error');
+    }
+    else {
+      $(divClass).addClass('has-error');
+      $(divClass).removeClass('has-success');
+    }
+  });
+}
+
+function expValidate(input, divClass) {
+  $(input).on('blur input', function () {
+    console.log($(input).val().split())
+    if (Stripe.card.validateExpiry($(input).val()) === true) {
+      $(divClass).addClass('has-success');
+      $(divClass).removeClass('has-error');
+    }
+    else {
+      $(divClass).addClass('has-error');
+      $(divClass).removeClass('has-success');
+    }
+  });
+}
+
+function copyShipping() {
+  $('.sameAs').change( function() {
+    if($(this).is(':checked')) {
+      console.log('checkbox')
+      $('.firstNameB').val($('.firstNameS').val());
+      $('.lastNameB').val($('.lastNameS').val());
+      $('.addressB').val($('.addressS').val());
+      $('.cityB').val($('.cityS').val());
+      $('.dropDownB').val($('.dropDownS').val());
+      $('.zipB').val($('.zipS').val());
+
+    }
+  });
+}
+function nameCityAddressValidation () {
+  textValidate('.firstNameS','.firstNameShipping');
+  textValidate('.lastNameS','.lastNameShipping');
+  textValidate('.addressS','.addressShipping');
+  textValidate('.cityS', '.cityShipping');
+  textValidate('.firstNameB','.firstNameBilling');
+  textValidate('.lastNameB','.lastNameBilling');
+  textValidate('.addressB','.addressBilling');
+  textValidate('.cityB', '.cityBilling');
+};
+
+function stateValidations() {
+  stateValidate('.dropDownS', '.stateShipping');
+  stateValidate('.dropDownB', '.stateBilling')
+};
+
+//$('billingClass').val($('shippingClass').val())
